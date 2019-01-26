f387a8a9abe041c0bdbdccb9c41ad46b6bdd9265
airfoof <airfoof@gmail.com>
2019-01-26 14:18:18 -0600

    updated UI to create a flashing effect when certain alert threadholds are reached.  Also added new functionality to use second button which send an MQTT message - used in my example to disable kids wifi.

--- medTrackerDisplay_esp8266.ino
+++ medTrackerDisplay_esp8266.ino
@@ -15,10 +15,19 @@ EasyButton button2(button2Pin);
 
 // Update these with values suitable for your network.
 const char* mqtt_server = "192.168.1.10";
-const char* clientId = "medtracker-esp8266";
+const char* clientId = "medtracker-esp8266-kitchen";
 const char* drugTimeTopic = "medtracker/drugtime";
 const char* medsNotTakenTopic = "medtracker/medsnottaken";
 const char* medsTakenTopic = "medtracker/medstaken";
+const char* accessPointName = "ESP8266-WiFi-Setup";
+const char* toggleInternetTopic = "kidtime/internet";
+const char* kidsInternetStatusTopic = "kidtime/internet/status";
+
+bool bFlashDisplay = false;
+bool bIsDisplayInverted = false;
+unsigned long startMillis;
+unsigned long currentMillis;
+const unsigned long flashDisplayDelay = 1000;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -35,12 +44,21 @@ void callback(String topic, byte* payload, unsigned int length) {
         displayDrugTimeImage();
       }else if(message == "off"){
         displayDrugsTakenImage();
-        delay(1000);
+        delay(2000);
         clearDisplay();
       }
-  }
-  if(topic == medsNotTakenTopic){
+  }else if(topic == medsNotTakenTopic){
       displayMedsNotTakenImage(message);
+  }else if(topic == kidsInternetStatusTopic){
+      if(message == "ON"){        
+        displayWIFIOnImage();
+        delay(2000);
+        clearDisplay();
+      }else if(message == "OFF"){
+        displayWIFIOffImage();
+        delay(2000);
+        clearDisplay();
+      }
   }
 }
 
@@ -51,6 +69,18 @@ void displayText(String text) {
   display.display();
 }
 
+void displayWIFIOnImage() {
+  display.clear();
+  display.drawXbm(0, 0, wifiOn_icon_width, wifiOn_icon_height, wifiOn_icon_bits);
+  display.display();
+}
+
+void displayWIFIOffImage() {
+  display.clear();
+  display.drawXbm(0, 0, wifiOff_icon_width, wifiOff_icon_height, wifiOff_icon_bits);
+  display.display();
+}
+
 void displayDrugTimeImage() {
   display.clear();
   display.drawXbm(0, 0, pill_small_icon_width, pill_small_icon_height, pill_small_icon_bits);
@@ -58,37 +88,57 @@ void displayDrugTimeImage() {
 }
 
 void displayDrugsTakenImage() {
+  bFlashDisplay = false;
+  display.normalDisplay();
   display.clear();
   display.drawXbm(0, 0, check_circle_width, check_circle_height, check_circle_bits);
   display.display();
 }
+
 void displayMedsNotTakenImage(String priority) {
   display.clear();
   if(priority == "1"){
     display.drawXbm(0, 0, alarm_icon_width, alarm_icon_height, alarm_icon_bits);
   }else if(priority == "2"){
+    bFlashDisplay = true;
     display.drawXbm(0, 0, alert_icon_width, alert_icon_height, alert_icon_bits);
   }else if(priority == "3"){
+    bFlashDisplay = true;
     display.drawXbm(0, 0, alert_icon_width, alert_icon_height, alert_icon_bits);
   }
   display.display();
 }
 
+void flashDisplay(){
+  currentMillis = millis();
+  if(bFlashDisplay){
+    if ((currentMillis - startMillis) >= flashDisplayDelay && bIsDisplayInverted == false)
+    {
+      startMillis = currentMillis;
+      display.invertDisplay();
+      bIsDisplayInverted = true;
+    }else if ((currentMillis - startMillis) >= flashDisplayDelay)
+    {
+      startMillis = currentMillis;
+      display.normalDisplay();
+      bIsDisplayInverted = false;
+    } 
+  }
+}
+
 void clearDisplay() {
+  bFlashDisplay = false;
+  display.normalDisplay();
   display.clear();
   display.display();
 }
 
 void handleButton1Press() {
-  //displayText("Button1 pressed!");
-  Serial.println("Button1 pressed!");
-
   client.publish(medsTakenTopic, clientId);
 }
 
 void handleButton2Press() {
-  //displayText("Button2 pressed!");
-  Serial.println("Button2 pressed!");
+  client.publish(toggleInternetTopic, "TOGGLE");
 }
 
 void reconnect() {
@@ -100,6 +150,7 @@ void reconnect() {
       // ... and resubscribe
       client.subscribe(drugTimeTopic);
       client.subscribe(medsNotTakenTopic);
+      client.subscribe(kidsInternetStatusTopic);
     } else {
       // Wait 5 seconds before retrying
       delay(5000);
@@ -112,10 +163,11 @@ void setup() {
   // Initialising the UI will init the display too.
   display.init();
   display.setFont(ArialMT_Plain_10);
+  display.flipScreenVertically();
   
   displayText("Setting up WiFi...");
   WiFiManager wifiManager;
-  wifiManager.autoConnect("AutoConnectAP");
+  wifiManager.autoConnect(accessPointName);
   
   displayText("Setting up MQTT...");
   client.setServer(mqtt_server, 1883);
@@ -135,6 +187,8 @@ void loop() {
 
   button1.read();
   button2.read();
+
+  flashDisplay();
 
   client.loop();
 }
